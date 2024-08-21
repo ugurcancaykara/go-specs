96def6c9b7d0c61f69fcf9cfe5f81c805cd79134
Ugurcan Caykara <ugurcancaykaradev@gmail.com>
2024-08-21 08:58:10 +0300

    chore: exampletest definition

--- learn-go-with-tests/02iterators/iterators.go
+++ learn-go-with-tests/02iterators/iterators.go
@@ -1,13 +1,9 @@
 package iterators
 
-const (
-	repeatedCount = 5
-)
-
-func Repeat(character string) string {
+func Repeat(character string, repeatCount int) string {
 	var repeated string
 
-	for i := 0; i < repeatedCount; i++ {
+	for i := 0; i < repeatCount; i++ {
 		repeated += character
 	}
 	return repeated
