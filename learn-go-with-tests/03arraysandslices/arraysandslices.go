f5d5c220248fd2733f6bbaeb57cafbb8a125dd57
Ugurcan Caykara <ugurcancaykaradev@gmail.com>
2024-08-21 10:04:58 +0300

    chore: write tests for arrays and slices

--- learn-go-with-tests/03arraysandslices/arraysandslices.go
+++ learn-go-with-tests/03arraysandslices/arraysandslices.go
@@ -7,3 +7,32 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	// lengthOfNumbers := len(numbersToSum)
+	// sums := make([]int, lengthOfNumbers)
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSumOfTails ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSumOfTails {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tails := numbers[1:]
+			sums = append(sums, Sum(tails))
+		}
+	}
+
+	return sums
+
+}
