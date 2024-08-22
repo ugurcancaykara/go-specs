6dabc52fe688868c368f6aceefe6fcc2fa8b7113
Ugurcan Caykara <ugurcancaykaradev@gmail.com>
2024-08-22 07:36:59 +0300

    feat: write interfaces tests with table driven tests

--- learn-go-with-tests/04structsmethodsinterfaces/structsmethodsinterfaces.go
+++ learn-go-with-tests/04structsmethodsinterfaces/structsmethodsinterfaces.go
@@ -30,3 +30,12 @@ func (c Circle) Perimeter() float64 {
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
--- learn-go-with-tests/04structsmethodsinterfaces/structsmethodsinterfaces_test.go
+++ learn-go-with-tests/04structsmethodsinterfaces/structsmethodsinterfaces_test.go
@@ -24,7 +24,7 @@ func TestPerimeter(t *testing.T) {
 
 }
 
-func TestArea(t *testing.T) {
+/* func TestArea(t *testing.T) {
 	checkArea := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
 		got := shape.Area()
@@ -42,4 +42,24 @@ func TestArea(t *testing.T) {
 		checkArea(t, circle, 314.1592653589793)
 
 	})
+} */
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			// We can change our error message into %#v got %g want %g. The %#v format string will print out our struct with the values in its field, so the developer can see at a glance the properties that are being tested.
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+
 }
