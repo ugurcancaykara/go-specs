02d3e7a7edd5ac2054f0e6f456f64d20aeada32e
Ugurcan Caykara <ugurcancaykaradev@gmail.com>
2024-08-22 07:47:57 +0300

    refactor: convert to subtests
    By wrapping each case in a t.Run you will have clearer test output on failures
    as it will print the name of the case

--- learn-go-with-tests/04structsmethodsinterfaces/structsmethodsinterfaces_test.go
+++ learn-go-with-tests/04structsmethodsinterfaces/structsmethodsinterfaces_test.go
@@ -46,20 +46,23 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
+		name    string
 		shape   Shape
 		hasArea float64
 	}{
-		{shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.10},
-		{shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+		{name: "rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.hasArea {
-			// We can change our error message into %#v got %g hasArea %g. The %#v format string will print out our struct with the values in its field, so the developer can see at a glance the properties that are being tested.
-			t.Errorf("got %v hasArea %g", got, tt.hasArea)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				// We can change our error message into %#v got %g hasArea %g. The %#v format string will print out our struct with the values in its field, so the developer can see at a glance the properties that are being tested.
+				t.Errorf("got %v hasArea %g", got, tt.hasArea)
+			}
+		})
 	}
 
 }
