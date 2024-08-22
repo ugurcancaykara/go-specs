052d21795bfb125c3d44d143b6c4e6373682d936
Ugurcan Caykara <ugurcancaykaradev@gmail.com>
2024-08-22 20:06:32 +0300

    refactor: let's make a quick test helper for our error check
    to improve the test's readability -> assertError

--- learn-go-with-tests/05pointersanderrors/pointersanderrors_test.go
+++ learn-go-with-tests/05pointersanderrors/pointersanderrors_test.go
@@ -6,12 +6,21 @@ import (
 
 func TestWallet(t *testing.T) {
 
+	// test helpers
+
 	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
 		t.Helper()
 		got := wallet.Balance()
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
 		}
 	}
 	t.Run("deposit", func(t *testing.T) {
@@ -34,11 +43,9 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
 
+		assertError(t, err)
 		assertBalance(t, wallet, startingBalance)
 
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
-		}
 	})
 
 }
