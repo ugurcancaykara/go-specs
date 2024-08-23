dfec5ff4f02f3af38efe9f3b6964b986eb239558
Ugurcan Caykara <ugurcancaykaradev@gmail.com>
2024-08-23 06:51:29 +0300

    feat: write test for unknown word
    The way to handle this scenario in Go is to return a second argument
    which is an Error type
    assert the error message we first check that the error is not nil and
    then use .Error() method to get the string which we can then pass to
    the assertion.

--- learn-go-with-tests/06maps/dictionary_test.go
+++ learn-go-with-tests/06maps/dictionary_test.go
@@ -3,15 +3,30 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
+
 	// dictionary := map[string]string{"test": "this is just a test"}
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
 
-	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
-	}
+		got := dictionary.Search("test")
+		want := "this is just a test"
+
+		if got != want {
+			t.Errorf("got %q want %q given, %q", got, want, "test")
+		}
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 
 }
 
