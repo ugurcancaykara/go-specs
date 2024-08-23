b82eb71ed6557533ab53226d990e2a43626d7bb2
Ugurcan Caykara <ugurcancaykaradev@gmail.com>
2024-08-23 07:51:03 +0300

    refactor: turning errors to constant errors

--- learn-go-with-tests/06maps/dictionary.go
+++ learn-go-with-tests/06maps/dictionary.go
@@ -1,14 +1,15 @@
 package main
 
-import (
-	"errors"
-)
-
 type Dictionary map[string]string
+type DictionaryErr string
 
-var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 )
 
 func (d Dictionary) Search(word string) (string, error) {
