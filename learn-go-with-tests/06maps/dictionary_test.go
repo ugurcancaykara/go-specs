7330ad1ebeb1979dc210d58434b43dd1d887fe68
Ugurcan Caykara <ugurcancaykaradev@gmail.com>
2024-08-23 07:58:31 +0300

    feat: write test for new word at update method

--- learn-go-with-tests/06maps/dictionary_test.go
+++ learn-go-with-tests/06maps/dictionary_test.go
@@ -52,14 +52,28 @@ func TestAdd(t *testing.T) {
 }
 
 func TestUpdate(t *testing.T) {
-	word := "test"
-	definition := "this is just a test"
-	dictionary := Dictionary{word: definition}
-	newDefinition := "new definition"
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new definition"
 
-	dictionary.Update(word, newDefinition)
+		err := dictionary.Update(word, newDefinition)
 
-	assertDefinition(t, dictionary, word, newDefinition)
+		assertErrors(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertErrors(t, err, ErrWordDoesNotExist)
+	})
 }
 
 func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
