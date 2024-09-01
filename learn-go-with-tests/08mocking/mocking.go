d51faa4383aff78fd3612ef1ec2117a7ef730010
Ugurcan Caykara <ugurcancaykaradev@gmail.com>
2024-09-01 15:03:59 +0300

    fix(mocking): write enough code to make it pass

--- learn-go-with-tests/08mocking/mocking.go
+++ learn-go-with-tests/08mocking/mocking.go
@@ -1,6 +1,9 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"fmt"
+)
 
 func main() {
 
@@ -8,5 +11,5 @@ func main() {
 
 // 2- Write the minimal amount of code for the test to run and check the failing test output
 func Countdown(out *bytes.Buffer) {
-
+	fmt.Fprint(out, "3")
 }
