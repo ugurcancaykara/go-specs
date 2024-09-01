a2f3617d1297b2040e37aed5913f2017f7d92e56
Ugurcan Caykara <ugurcancaykaradev@gmail.com>
2024-09-01 15:34:35 +0300

    feat(mocking): update the tests to inject a dependency on our Spy
    and assert that the sleep has been called 3 times

--- learn-go-with-tests/08mocking/mocking_test.go
+++ learn-go-with-tests/08mocking/mocking_test.go
@@ -8,9 +8,11 @@ import (
 // 1- Write the test first
 
 func TestCountdown(t *testing.T) {
+	// Update the tests to inject a dependency on our Spy and assert that the sleep has been called 3 times.
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	want := `3
@@ -20,5 +22,9 @@ Go!`
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
 	}
 }
