9030b4560e6de0d023d8ba31824178034a677c67
Ugurcan Caykara <ugurcancaykaradev@gmail.com>
2024-09-01 15:41:43 +0300

    fix(mocking3): write enough code to make it pass

--- learn-go-with-tests/08mocking/mocking.go
+++ learn-go-with-tests/08mocking/mocking.go
@@ -29,10 +29,24 @@ func (s *SpySleeper) Sleep() {
 
 // Spies are a kind of mock which can record how a dependency is used. They can record the arguments sent in, how many times it has been called, etc. In our case, we're keeping track of how many times Sleep() is called so we can check it in our test.
 
+// Let's create a real sleeper which implements the interface we need
+
+type DefaultSleeper struct {
+}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 func main() {
 
 	// 5- To complete matters, let's now wire up our function into a `main` so we have some working software to reassure ourselves we're making progress.
-	Countdown(os.Stdout)
+	// Countdown(os.Stdout)
+
+	// Mocking3
+	// We can then use it in our real application like so
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 
 	// conclusion: Yes this seems trivial but this approach is what is recommended for any project. Take a thin slice of functionality and make it work end-to-end, backed by tests.
 }
@@ -45,7 +59,7 @@ func Countdown(out io.Writer, sleeper Sleeper) { // 4- we know *bytes.Buffer wor
 
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 
 	// NOTE: Now tests still pass and the software works as intended but we have some problems:
